db8805fbd80003690cca36de3a4a5cddda0399c5
PAFT1501 <pedro.a.f.teixeira@tecnico.ulisboa.pt>
2020-04-30 19:40:37 +0100

    AsymmetricCrypto methods test

--- Client/src/test/java/sec/project/client/AppTest.java
+++ Client/src/test/java/sec/project/client/AppTest.java
@@ -1,20 +1,50 @@
 package sec.project.client;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
+import sec.project.library.AsymmetricCrypto;
+
+import javax.crypto.Cipher;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+
+    KeyStore keyStore;
+    PrivateKey privatekey;
+    PublicKey publicKey;
+
+    @Before
+    public void populateForTest(){
+        try {
+            keyStore = AsymmetricCrypto.getKeyStore("data/keys/client1_keystore.jks", "client1password");
+            privatekey = AsymmetricCrypto.getPrivateKey(keyStore, "client1password", "client1");
+            publicKey = AsymmetricCrypto.getPublicKeyFromCert("data/keys/client1_certificate.crt");
+        }catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
     }
+
+
+    @Test
+    public void wrapAndValidateDigitalSignatureTest(){
+        try {
+            String test = "ola";
+            byte[] bytes = AsymmetricCrypto.wrapDigitalSignature(test, privatekey);
+            assertTrue(AsymmetricCrypto.validateDigitalSignature(bytes, publicKey, test));
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
